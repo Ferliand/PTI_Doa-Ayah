e5271414e12c7558228830280cd3a246a354f6bb
Ferli Andriansyah <103373638+Ferliand@users.noreply.github.com>
2023-10-29 14:53:48 +0700

    migrate table arsip

--- database/migrations/create_arsips_table.php
+++ database/migrations/create_arsips_table.php
@@ -9,19 +9,28 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('arsips', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_arsip');
+            $table->string('kode_arsip');
+            $table->string('perihal');
+            $table->string('kategori');
+            $table->date('tanggal_terbit');
+            $table->date('tanggal_selesai');
+            $table->string('lokasi_arsip');
+            $table->string('file');
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('arsips');
+        Schema::dropIfExists('arips');
     }
 };
